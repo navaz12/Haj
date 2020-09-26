518cf353a061009c258f373469b7ef3ae2a5343f
Navaz Naz <47378614+navaz12@users.noreply.github.com>
2020-09-26 20:05:27 +0300

    Update contact.php
    email updated

--- forms/contact.php
+++ forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'muhammednavazmk@gmail.com.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'https://script.google.com/macros/s/AKfycbwX2Pue09CopU9HQ01P7mTuAuPZ9FrdYZH4LDtyO-Vvw79y3m0/exec' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
